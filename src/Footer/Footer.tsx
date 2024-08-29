24f6e8d2f821d7e8f3856c895417b1a25daacb63
BGMarinho <bgmarinho96@gmail.com>
2024-08-29 20:35:16 -0300

    feature (Footer): Adding a simple Footer component.

---
+++ src/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import './styles.css';
+
+export default function Footer() {
+  return (
+    <section className="footer-wrapper">
+      <div className="informetion">
+        <span>
+          Copyright © 2024 <a href="https://github.com/BGMarinho">BGMarinho</a>
+          . Todos os direitos reservados.
+        </span>
+      </div>
+    </section>
+  );
+}
