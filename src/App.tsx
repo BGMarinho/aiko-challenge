bbf4525c60b02b668a25fc3df0f8277811c207a5
BGMarinho <bgmarinho96@gmail.com>
2024-08-30 16:25:10 -0300

    feature (App.tsx): Implementing some logic to the JSX for showing the last position of each equipment on the map.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+// import Equipment from '../data/equipment.json';
+import { getEquipmentsLastPositions } from '../hooks/useEquipment.ts';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import Aiko from '../images/aiko.png';
 import Footer from './Footer';
 import './App.css';
 
 export default function App() {
+  const equipments = getEquipmentsLastPositions();
+  console.log(equipments);
+
   return (
     <div className="map-container">
       <img className="aiko-logo" src={Aiko} alt="aiko-logo" />
@@ -11,17 +16,24 @@ export default function App() {
         className="map"
         center={[51.505, -0.09]}
         zoom={10}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {equipments.map((equipmentLastPosition, index) => {
+          return (
+            <Marker
+              key={index}
+              position={[equipmentLastPosition.lat, equipmentLastPosition.lon]}
+            >
+              <Popup>
+                teste linha 1 <br /> teste linha 2
+              </Popup>
+            </Marker>
+          );
+        })}
       </MapContainer>
       <Footer />
     </div>
