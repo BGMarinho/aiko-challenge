b37e60a0bcb31c76b0e8c8efc002359339b17f16
BGMarinho <bgmarinho96@gmail.com>
2024-08-30 19:43:11 -0300

    feature (useEquipment): Implementing a basic version of the hook.

--- hooks/useEquipment.ts
+++ hooks/useEquipment.ts
@@ -1,12 +1,20 @@
-import EquipmentPositions from '../data/equipmentPositionHistory.json';
+import EquipmentPositionHistory from '../data/equipmentPositionHistory.json';
 import Equipments from '../data/equipment.json';
+import EquipmentModel from '../data/equipmentModel.json';
+import EquipmentState from '../data/equipmentState.json';
+import EquipmentStateHistory from '../data/equipmentStateHistory.json';
 
 export function getEquipmentsLastPositions() {
-  const equipmentsLastPositions = EquipmentPositions.map((equipment) => {
-    return equipment.positions[EquipmentPositions.length];
+  const equipmentsLastPositions = EquipmentPositionHistory.map((equipment) => {
+    return equipment.positions[EquipmentPositionHistory.length];
   });
   return equipmentsLastPositions;
 }
 
-export function equipments() {}
+export function equipments() {
+  const equipmentObj = EquipmentPositionHistory.map((equipment) => {
+    return equipment;
+  });
+  console.log(equipmentObj);
+}
 equipments();
